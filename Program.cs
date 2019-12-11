3a3d5e23c6d5c46aee5ed8ced575dd62969de7cf
Viktor Eriksson <mammovick@gmail.com>
2019-12-12 00:00:35 +0100

    add support for paths

--- Program.cs
+++ Program.cs
@@ -2,40 +2,79 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Data.Common;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace utorrent_migrate
 {
     class Program
     {
+        class Torrent {
+            public string name;
+            public string path;
+
+            public Torrent() {
+
+            }
+            public Torrent(string name, string path) {
+                this.name = name;
+                this.path = path;
+            }
+        }
+
         static void Main(string[] args)
         {
-            string roamingPath = Environment.GetEnvironmentVariable("appdata");
+            string roamingPath = Environment.GetEnvironmentVariable("appdata"); //Windows
             string uTorrentPath = Path.Combine(roamingPath, "uTorrent");
 
-            StreamReader sr = new StreamReader(Path.Combine(uTorrentPath, "resume.dat"));
+            StreamReader sr = new StreamReader(Path.Combine(uTorrentPath, "resume.dat"), Encoding.UTF8);
             string resumeData = sr.ReadToEnd();
             sr.Dispose();
 
-            MatchCollection matches = Regex.Matches(resumeData, ":caption\\d+:(.+?)5:");
-            IEnumerable<string> torrents =
-                from torrent in matches
-                select torrent.Groups[1].Captures[0].Value;
+            MatchCollection torrentData = Regex.Matches(resumeData, @":caption\d+:(.+?)5:.+?:path\d+:(.+?)6:");
+            List<Torrent> torrentList = new List<Torrent>();
 
-            if(!Directory.Exists("torrents")) {
+            foreach (Match match in torrentData) {
+                Torrent torrent = new Torrent();
+                torrent.name = match.Groups[1].ToString();
+                torrent.path = match.Groups[2].ToString();
+                torrentList.Add(torrent);
+            }
+
+            if (!Directory.Exists("torrents")) {
                 Directory.CreateDirectory("torrents");
             }
 
             int successful = 0;
-            foreach(string torrent in torrents) {
-                string combinedTorrentPath = Path.Combine(uTorrentPath, torrent + ".torrent");
-                if(File.Exists(combinedTorrentPath)) {
-                    File.Copy(combinedTorrentPath, "torrents/" + torrent + ".torrent");
-                    ++successful;
+            using (StreamWriter sw = new StreamWriter("torrents/paths.txt", false)) {
+                foreach (Torrent torrent in torrentList.OrderBy(p => p.name)) {
+                    string combinedTorrentPath = Path.Combine(uTorrentPath, torrent.name + ".torrent");
+
+                    if (File.Exists(combinedTorrentPath)) {
+                        string outFile = "torrents/" + torrent.name + ".torrent";
+
+                        if (!File.Exists(outFile)) {
+                            File.Copy(combinedTorrentPath, outFile);
+                            sw.WriteLine(String.Format("{0} -> {1}", torrent.name, torrent.path));
+
+                            ++successful;
+                        } else {
+                            Console.WriteLine("{0} already exists in /torrents. Skipping.", torrent.name);
+                        }
+                    }
                 }
             }
 
-            Console.WriteLine("Successful: {0}/{1}", successful, torrents.OfType<string>().Count());
+            Console.WriteLine(File.ReadAllText("torrents/paths.txt"));
+            Console.WriteLine();
+            Console.WriteLine("This information is also available in /torrents/paths.txt");
+            Console.WriteLine(".torrent files are stored in /torrents");
+            Console.WriteLine();
+            Console.WriteLine("Successful: {0}/{1}", successful, torrentList.OfType<Torrent>().Count());
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
     }
